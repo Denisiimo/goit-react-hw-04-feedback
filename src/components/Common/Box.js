833265417cf818dc8822ccc1149ccd3183116ab4
Denisiimo <denischorny22@gmail.com>
2023-07-22 17:09:53 +0100

    Test1

---
+++ src/components/Common/Box.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import { border, space, layout, flexbox, background } from 'styled-system';
+
+export const Box = styled('div')(border, space, layout, flexbox, background);
